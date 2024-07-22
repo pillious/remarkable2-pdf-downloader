a71e280abc5c9f40f78b3d07a519acf24b758425
Andrew <andrewzhlee@gmail.com>
2024-07-22 18:59:46 -0400

    Add cmd line flags & determine whether to download files based on modification times

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"flag"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"regexp"
@@ -12,8 +13,15 @@ import (
 )
 
 var (
-	baseUrl        = "http://10.11.99.1:80/"
-	backupLocation = "./mypdfbackups/"
+	backupsDir  string
+	baseUrl     string
+	includeList listFlags
+	excludeList listFlags
+	doLogToFile bool
+	verbose     bool
+
+	logFileName                 = ".backup.logs"
+	lastModifiedFileName string = ".last_modified.json"
 )
 
 type Document struct {
@@ -26,22 +34,50 @@ type Document struct {
 }
 
 func main() {
-	includeSet := make(map[string]struct{})
-	includeSet["uuuu"] = struct{}{}
-	includeSet["Quick sheets"] = struct{}{}
-	includeSet["ANTH 222"] = struct{}{}
-	excludeSet := make(map[string]struct{})
-	excludeSet["uuuu"] = struct{}{}
+	parseFlags()
+	makeDir(backupsDir)
 
-	traverseFileSystem(Stack[string]{}, "", &includeSet, &excludeSet)
+	if doLogToFile {
+		setupFileLogger(logFileName)
+	}
+	defer closeFileLogger()
+
+	includeSet := sliceToSet(includeList)
+	excludeSet := sliceToSet(excludeList)
+	modificationTimes := getLastModifiedTimes()
+
+	if verbose {
+		log.Println("Starting Downloads.")
+		log.Println("-------------------")
+	}
+
+	traverseFileSystem(Stack[string]{}, "", &includeSet, &excludeSet, modificationTimes)
+	updateLastModifiedTimes(modificationTimes)
+
+	if verbose {
+		log.Println("-------------------")
+		log.Println("Downloads complete.")
+	}
 }
 
-// The exclude set has priority over include set.
-func traverseFileSystem(path Stack[string], folderId string, include *map[string]struct{}, exclude *map[string]struct{}) {
+func parseFlags() {
+	flag.StringVar(&backupsDir, "backupsDir", ".", "The directory to store backups.")
+	flag.StringVar(&baseUrl, "url", "http://10.11.99.1:80", "Address of the Remarkable2 web UI.")
+
+	flag.Var(&includeList, "i", "A document/folder name or id to download. Add this flag multiple times to include multiple items.")
+	flag.Var(&excludeList, "e", "A document/folder name or id to skip. Add this flag multiple times to include multiple items. (Note: The exclude flag has higher precedence than the include flag)")
+
+	flag.BoolVar(&doLogToFile, "l", false, "Write logs to {backupsDir}/backup.logs instead of STDOUT.")
+	flag.BoolVar(&verbose, "v", false, "Enable verbose output.")
+
+	flag.Parse()
+}
+
+func traverseFileSystem(path Stack[string], folderId string, include *map[string]struct{}, exclude *map[string]struct{}, modificationTimes *map[string]string) {
 	isRoot := folderId == ""
 	pathStr := strings.Join(path, "/")
 
-	makeDir(backupLocation + pathStr)
+	makeDir(backupsDir + "/" + pathStr)
 
 	var docs []Document
 	if isRoot {
@@ -57,6 +93,9 @@ func traverseFileSystem(path Stack[string], folderId string, include *map[string
 		if _, ok := (*exclude)[doc.Name]; ok {
 			continue
 		}
+		if !didModificationTimeChange(doc.Id, doc.UpdatedAt, modificationTimes) {
+			continue
+		}
 
 		switch doc.Type {
 		case "DocumentType":
@@ -66,23 +105,28 @@ func traverseFileSystem(path Stack[string], folderId string, include *map[string
 				}
 			}
 
-			if isRoot {
-				fmt.Printf("%s ... ", doc.Name)
-			} else {
-				fmt.Printf("%s/%s ... ", pathStr, doc.Name)
+			if verbose {
+				if isRoot {
+					log.Printf("%s ...\n", doc.Name)
+				} else {
+					log.Printf("%s/%s ...\n", pathStr, doc.Name)
+				}
 			}
 
 			startTime := time.Now()
 			pdfName, content := downloadDocument(doc.Id)
 			duration := time.Since(startTime).Seconds()
-			fmt.Printf("time: %.2fs | size: %s\n", duration, formatSize(len(*content)))
+			if verbose {
+				log.Printf("time: %.2fs | size: %s\n", duration, formatSize(len(*content)))
+			}
 
-			createPdf(pdfName, content, backupLocation+pathStr)
+			createPdf(pdfName, content, backupsDir+"/"+pathStr)
+			(*modificationTimes)[doc.Id] = doc.UpdatedAt
 
 		case "CollectionType":
 			path.Push(doc.Name)
-			traverseFileSystem(path, doc.Id, include, exclude)
-			removeEmptyDir(backupLocation + strings.Join(path, "/"))
+			traverseFileSystem(path, doc.Id, include, exclude, modificationTimes)
+			removeEmptyDir(backupsDir + "/" + strings.Join(path, "/"))
 			path.Pop()
 		}
 	}
@@ -93,55 +137,55 @@ func getDocumentsAtRoot() *[]Document {
 }
 
 func getDocuments(folderId string) *[]Document {
-	url := baseUrl + "documents/" + folderId
+	url := baseUrl + "/documents/" + folderId
 
 	req, err := http.NewRequest("POST", url, nil)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 
 	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	defer res.Body.Close()
 
 	docs := &[]Document{}
 	err = json.NewDecoder(res.Body).Decode(docs)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	if res.StatusCode != http.StatusOK {
-		panic(res.StatusCode)
+		log.Panic(res.StatusCode)
 	}
 
 	return docs
 }
 
 func downloadDocument(documentId string) (string, *[]byte) {
-	url := baseUrl + "download/" + documentId + "/placeholder"
+	url := baseUrl + "/download/" + documentId + "/placeholder"
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 
 	client := &http.Client{}
 
 	res, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	if res.StatusCode != http.StatusOK {
-		fmt.Println(string(body))
-		panic(res.StatusCode)
+		log.Println(string(body))
+		log.Panic(res.StatusCode)
 	}
 
 	re := regexp.MustCompile(`"(.+)"$`)
@@ -151,15 +195,57 @@ func downloadDocument(documentId string) (string, *[]byte) {
 	return fileName, &body
 }
 
-func createPdf(fileName string, content *[]byte, path string) {
-	pdf, err := os.Create(path + "/" + fileName)
-	if err != nil {
-		panic(err)
-	}
-	defer pdf.Close()
+func getLastModifiedTimes() *map[string]string {
+	path := backupsDir + "/" + lastModifiedFileName
+	result := &map[string]string{}
 
-	_, err = pdf.Write(*content)
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
+	defer file.Close()
+	err = json.NewDecoder(file).Decode(result)
+	if err == io.EOF {
+		return result
+	} else if err != nil {
+		log.Panic(err)
+	}
+
+	return result
+}
+
+func updateLastModifiedTimes(modificationTimes *map[string]string) {
+	path := backupsDir + "/" + lastModifiedFileName
+
+	file, err := os.Create(path)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+
+	result, err := json.Marshal(modificationTimes)
+	if err != nil {
+		log.Panic(err)
+	}
+	file.WriteString(string(result))
+}
+
+func didModificationTimeChange(id string, newTime string, modificationTimes *map[string]string) bool {
+	if _, ok := (*modificationTimes)[id]; !ok {
+		return true
+	}
+
+	layout := "2006-01-02T15:04:05.000Z"
+	t1, err := time.Parse(layout, (*modificationTimes)[id])
+	if err != nil {
+		log.Println(err)
+		return true
+	}
+	t2, err := time.Parse(layout, newTime)
+	if err != nil {
+		log.Println(err)
+		return true
+	}
+
+	return t1.Before(t2)
 }
--- utils.go
+++ utils.go
@@ -2,18 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
-func makeDir(path string) {
-	err := os.MkdirAll(path, 0755)
+var logFile *os.File
+
+func setupFileLogger(fileName string) string {
+	filePath := backupsDir + "/" + fileName
+
+	var err error
+	logFile, err = os.OpenFile(filePath, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
+	}
+
+	log.SetOutput(logFile)
+
+	return filePath
+}
+
+func closeFileLogger() {
+	if logFile != nil {
+		log.SetOutput(os.Stdout)
+		err := logFile.Close()
+		if err != nil {
+			log.Println("Error closing log file:", err)
+		}
+		log.Printf("Logs written to %s\n", logFile.Name())
 	}
 }
 
-func removeEmptyDir(path string) {
-	os.Remove(path)
+func sliceToSet(lst []string) map[string]struct{} {
+	set := make(map[string]struct{})
+	for _, s := range lst {
+		s = strings.TrimSpace(s)
+		set[s] = struct{}{}
+	}
+	return set
 }
 
 func formatSize(size int) string {
@@ -37,4 +64,39 @@ func formatSize(size int) string {
 	default:
 		return fmt.Sprintf("%.2f TB", float64(size)/TB)
 	}
+}
+
+func makeDir(path string) {
+	err := os.MkdirAll(path, 0755)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func removeEmptyDir(path string) {
+	os.Remove(path)
+}
+
+func createPdf(fileName string, content *[]byte, path string) {
+	pdf, err := os.Create(path + "/" + fileName)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer pdf.Close()
+
+	_, err = pdf.Write(*content)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+type listFlags []string
+
+func (l *listFlags) String() string {
+	return strings.Join(*l, ", ")
+}
+
+func (l *listFlags) Set(value string) error {
+	*l = append(*l, value)
+	return nil
 }
